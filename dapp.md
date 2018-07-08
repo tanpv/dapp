774fff9c9fa49e2e96c3d26fe8244d7b9fcae59b
tanpham <phamtan500@gmail.com>
2018-07-08 20:46:05 +0700

    update map

---
+++ dapp.md
@@ -0,0 +1,11 @@
+## The big picture
+
+## Installation
+
+## Truffle framework
+
+## Web3.js
+
+## Smart Contract
+
+## Automation Test
--- election/contracts/election.sol
+++ election/contracts/election.sol
@@ -1,9 +1,28 @@
 pragma solidity ^0.4.24;
 
 contract Election {
-  string public candidate;
+  // Candidate model
+  struct Candidate {
+    uint id;
+    string name;
+    uint voteCount;
+  }
+
+  // store candidate
+  mapping(uint => Candidate) public candidates;
+  uint public candidatesCount;
+
+  // store voter
+  mapping(address => bool) public voters;
+  
   constructor () public {
-    candidate = "candidate 1";
+    addCandidate("Candidate 1");
+    addCandidate("Candidate 2");
+  }
+
+  function addCandidate(string _name) private {
+    candidatesCount ++;
+    candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
   }
 }
 
