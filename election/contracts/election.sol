0e55d052d7ef4914c0891746e3dbc76c1cf21f2e
tanpham <phamtan500@gmail.com>
2018-07-09 00:56:18 +0700

    add test

--- election/contracts/election.sol
+++ election/contracts/election.sol
@@ -10,10 +10,9 @@ contract Election {
 
   // store candidate
   mapping(uint => Candidate) public candidates;
-  uint public candidatesCount;
-
-  // store voter
+  // store vote state
   mapping(address => bool) public voters;
+  uint public candidatesCount;
   
   constructor () public {
     addCandidate("Candidate 1");
@@ -23,6 +22,20 @@ contract Election {
   function addCandidate(string _name) private {
     candidatesCount ++;
     candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+  }
+
+  function vote (uint _candidateId) public {
+    // require not yet vote
+    require(!voters[msg.sender]);
+
+    // require a valid candidate
+    require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+    // record voter already voted
+    voters[msg.sender] = true;
+
+    // update candidate vote count
+    candidates[_candidateId].voteCount ++;
   }
 }
 
--- election/test/election.js
+++ election/test/election.js
@@ -28,4 +28,20 @@ contract("Election", function(accounts){
     });
   });
 
+  it("allow a voter to cast a vote", function() {
+    return Election.deployed().then(function(instance) {
+      electionInstance = instance;
+      candidateId =1;
+      return electionInstance.vote(candidateId, {from: accounts[0]});
+    }).then(function(receipt) {
+      return electionInstance.voters(accounts[0]);
+    }).then(function(voted) {
+      assert.equal(voted, true, "already voted");
+      return electionInstance.candidates(candidateId);
+    }).then(function(candidate) {
+      var voteCount = candidate[2];
+      assert.equal(voteCount, 1, "increase vote");
+    });
+  });
+
 });
